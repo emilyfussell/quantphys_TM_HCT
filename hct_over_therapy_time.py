5f15408eb6ccd864a5d44ccbba925d533955656b
emilyfussell <efuss2019@gmail.com>
2023-04-17 13:08:31 -0400

    rename to match model

--- hct_over_therapy_time.py
+++ hct_over_therapy_time.py
@@ -7,14 +7,17 @@ beta_3 = -0.11
 beta_4 = 0.44
 beta_5 = 0.10
 
-time_recen = np.linspace(-20, 10, 1500) # recentered time value, with beginning of HRT at 0
-post_time = np.linspace(0, 10, 1500) # time after beginning of HRT
+time_recen = np.linspace(-20, 10, 1500)  # recentered time value, with beginning of HRT at 0
+post_time = np.linspace(0, 10, 1500)  # time after beginning of HRT
 # hematocrit equation from longitudinal paper - individual factors are negated as looking at average
 # the fudge factor is 45 (or intercept value) to account for beginning HCT values
-hct_TM = 45 + beta_1 + (beta_2 * time_recen) + (beta_3 * post_time) + (beta_4 * time_recen) + (beta_5 * post_time)
+# use m2 bc it adjusted for most recent Hct result before HT initiation
+"""hct_TM_m1 = 45 + -0.07 + (0.06 * time_recen) + (-0.13 * post_time) + (0.42 * time_recen) + (0.13 * post_time)"""
+hct_TM_m2 = 45 + beta_1 + (beta_2 * time_recen) + (beta_3 * post_time) + (beta_4 * time_recen) + (beta_5 * post_time)
 # plot graph of TM Hct pop averages
-plt.plot(time_recen, hct_TM, label="Beta5 = 0.10, experimental value")
-plt.xlabel("Time, 0 = beginning of HT")
+"""plt.plot(time_recen, hct_TM_m1, label = "model 1")"""
+plt.plot(time_recen, hct_TM_m2, label="model 2")
+plt.xlabel("Time in years, 0 = beginning of HT")
 plt.ylabel("Average Hct percentage")
 plt.title("Population Avg of Hct Values for TM people before and after HT")
 plt.show()
@@ -26,7 +29,7 @@ hct_TM_sa2 = 45 + beta_1 + (beta_2 * time_recen) + (beta_3 * post_time) + (beta_
 hct_TM_sa3 = 45 + beta_1 + (beta_2 * time_recen) + (beta_3 * post_time) + (beta_4 * time_recen) + (0.11 * post_time)
 hct_TM_sa4 = 45 + beta_1 + (beta_2 * time_recen) + (beta_3 * post_time) + (beta_4 * time_recen) + (0.12 * post_time)
 # plot sensitivity analysis
-plt.plot(time_recen, hct_TM, label="Beta5 = 0.10, experimental value")
+plt.plot(time_recen, hct_TM_m2, label="Beta5 = 0.10, experimental value")
 plt.plot(time_recen, hct_TM_sa1, label="Beta5 = 0.08")
 plt.plot(time_recen, hct_TM_sa2, label="Beta5 = 0.09")
 plt.plot(time_recen, hct_TM_sa3, label="Beta5 = 0.11")
